fbfe9e93fd5f17f8de38d59a9c7f10e9af06335e
mohit sharma <sharm163@umn.edu>
2015-09-13 17:45:10 -0500

    added hinge less with user shift

---
+++ src/modelHingeWUm.h
@@ -0,0 +1,21 @@
+#ifndef _MODEL_HINGE_UM_H_
+#define _MODEL_HINGE_UM_H_
+ 
+#include "model.h"
+
+typedef struct {
+  Model proto;
+  float *u_m;
+  float rhoRMS;
+  float regUm;
+} ModelHingeWUm;
+
+
+float ModelHingeWUm_setScore(void *self, int u, int *set, int setSz, 
+    float**sim);
+float ModelHingeWUm_objective(void *self, Data *data, float **sim);
+void ModelHingeWUm_train(void *self, Data *data, Params *params, float **sim,
+    ValTestRMSE *valTest);
+void modelHingeWUm(Data *data, Params *params, ValTestRMSE *valTest);
+
+#endif
