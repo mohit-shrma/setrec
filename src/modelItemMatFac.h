06b184e9173299e56b5733a74bb88f816865042a
mohit sharma <sharm163@umn.edu>
2015-09-27 13:03:37 -0500

    added method to header file

--- src/modelItemMatFac.h
+++ src/modelItemMatFac.h
@@ -13,6 +13,7 @@ extern Model ModelMajorityProto;
 
 void ModelItemMatFac_train(void *self, Data *data, Params *params, float **sim, ValTestRMSE *valTest);
 void ModelItemMatFac_trainRMSProp(void *self, Data *data, Params *params, float **sim, ValTestRMSE *valTest);
+void ModelItemMatFac_trainSamp(void *self, Data *data, Params *params, float **sim, ValTestRMSE *valTest);
 float ModelItemMatFac_validationErr(void *self, Data *data, float **sim);
 float ModelItemMatFac_objective(void *self, Data *data, float **sim);
 void modelItemMatFac(Data *data, Params *params, ValTestRMSE *valTest);
