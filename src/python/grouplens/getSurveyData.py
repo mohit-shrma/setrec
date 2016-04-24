3e4a46608d8ae78241ce5abab16c6aa1e3155a94
mohit sharma <sharm163@umn.edu>
2016-04-24 17:09:40 -0500

    updated code for grouplens data retrieval and cleanup

--- src/python/grouplens/getSurveyData.py
+++ src/python/grouplens/getSurveyData.py
@@ -1,8 +1,72 @@
 import sys
 import MySQLdb
+import datetime
+
+def getInvalidUsersWRat(uiRatings, uSetRatings):
+  invalidUsers = set([])
+  for user, itemsSetsRating in uSetRatings.iteritems():
+    for itemSetRating in itemsSetsRating:
+      items = itemSetRating[0]
+      rating = itemSetRating[1]
+      for item in items:
+        if item not in uiRatings[user]:
+          invalidUsers.add(user)
+          break
+      if user in invalidUsers:
+        break
+  print 'Ratings not found for: ', len(invalidUsers)
+  return invalidUsers
+
+
+def getInvalidUsersByTS(setFName):
+  #order ratings for user in set by tstamp, then remove those users who rated
+  #set with less than one sec bw sets, 
+  #read sorted file by users
+  invalidUsers = []
+  with open(setFName, 'r') as f:
+    prevUser = None
+    uTStamps = []
+    for line in f:
+      cols = line.strip().split(',')
+      currUser = int(cols[0])
+      currUItems = map(int, cols[1].split('-'))
+      currRat = float(cols[2])
+      strTs = cols[3]
+      currTs = datetime.datetime.strptime(strTs, "%Y-%m-%d %H:%M:%S")
+      if prevUser is not None and prevUser != currUser:
+        #found new user, analyze current user
+        uTStamps.sort()
+        #check if consecutive ts difference < 1000ms
+        for i in range(len(uTStamps)-1):
+          ts1 = uTStamps[i]
+          ts2 = uTStamps[i+1]
+          diff = ts2 - ts1
+          diffSecs = diff.total_seconds()
+          if diffSecs < 1:
+            invalidUsers.append(currUser)
+            break
+        #reset for next user 
+        uTStamps = []
+
+      uTStamps.append(currTs)
+      prevUser = currUser
+    #analyze last user
+    uTStamps.sort()
+    #check if consecutive ts difference < 1000ms
+    for i in range(len(uTStamps)-1):
+      ts1 = uTStamps[i]
+      ts2 = uTStamps[i+1]
+      diff = ts2 - ts1
+      diffSecs = diff.total_seconds()
+      if diffSecs < 1:
+        invalidUsers.append(currUser)
+        break
+  print 'no. of invalid users by TS: ', len(invalidUsers)
+  return set(invalidUsers)  
+
 
 def writeList(lst, opFName):
-  with (opFName, 'w') as g:
+  with open(opFName, 'w') as g:
     for l in lst:
       g.write(str(l) + '\n')
 
@@ -17,13 +81,39 @@ def getDBCursor():
   return cur
 
 
+def filtSetRatings(uSetRatings, invalUsers, opFileName):
+  nSetRatings = 0
+  with open(opFileName, 'w') as g:
+    for user, setRatings in uSetRatings.iteritems():
+      if user in invalUsers:
+        continue
+      for itemSetRating in setRatings:
+        itemSet = itemSetRating[0]
+        setRating = itemSetRating[1]
+        g.write(str(user) + ',' + '-'.join(map(str, itemSet)) + ',' +
+             str(setRating) + '\n')
+        nSetRatings += 1
+  print 'No. set ratings after filter: ', nSetRatings
+        
+
 def getSetRatings(cur, opFileName):
+  uSetRatings = {}
+  nSetRatings = 0
   #query to get all the data
-  cur.execute('select * from expt_set_user_rating')
+  cur.execute('select * from expt_set_user_rating order by userId, tstamp')
   with open(opFileName, 'w') as g:
     for row in cur.fetchall():
+      user = int(row[1])
+      setItems = map(int, row[2].split('-'))
+      setRating = float(row[3])
+      if user not in uSetRatings:
+        uSetRatings[user] = []
+      uSetRatings[user].append((setItems, setRating))
+      nSetRatings += 1
       strRow = map(str, row)
-      g.write(','.join(strRow) + '\n')
+      g.write(','.join(strRow[1:]) + '\n')
+  print 'nSetRatings: ', nSetRatings
+  return uSetRatings
 
 
 def getUserItems(uSetCSVFName):
@@ -32,8 +122,8 @@ def getUserItems(uSetCSVFName):
   with open(uSetCSVFName, 'r') as f:
     for line in f:
       cols = line.strip().split(',')
-      user = int(cols[1])
-      uItems = map(int, cols[2].split('-'))
+      user = int(cols[0])
+      uItems = map(int, cols[1].split('-'))
       users.add(user)
       for uItem in uItems:
         items.add(uItem)
@@ -45,11 +135,23 @@ def getUserItemRatings(cur, users, items, opFileName):
   cur.execute('select A.userId, A.movieId, A.rating, A.tstamp from \
       user_rating_pairs A INNER JOIN expt_set_user_rating B ON \
       A.userId=B.userId;')
+  uIRat = {}
+  nnz = 0
   with open(opFileName, 'w') as g:
     for row in cur.fetchall():
-      if row[0] in users and row[1] in items:
-        strRow = map(str, row)
-        g.write(','.join(strRow) + '\n')
+      user   = row[0]
+      item   = row[1]
+      rating = row[2]
+      if user in users and item in items:
+        if user not in uIRat:
+          uIRat[user] = {}
+        if item not in uIRat[user]:
+          uIRat[user][item] = rating
+          nnz += 1
+          strRow = map(str, row)
+          g.write(','.join(strRow) + '\n')
+  print 'no. of uIRatings: ', nnz
+  return uIRat
 
 
 def main():
@@ -57,20 +159,29 @@ def main():
   cur = getDBCursor()
   
   setFName = opPrefix + '_set_ratings.csv'
-  getSetRatings(cur, setFName)
+  uSetRatings = getSetRatings(cur, setFName)
   
   (users, items) = getUserItems(setFName)
-  
   print 'nUsers: ', len(users), ' nItems: ', len(items)
 
   writeList(list(users), opPrefix + '_set_users.txt')
   writeList(list(items), opPrefix + '_set_items.txt')
 
   uiRatFName = opPrefix + '_ui_ratings.csv'
-  getUserItemRatings(cur, users, items, uiRatFName)
+  uiRat = getUserItemRatings(cur, users, items, uiRatFName)
+
+  invalUserByTS = getInvalidUsersByTS(setFName)
+  invalUserByRat = getInvalidUsersWRat(uiRat, uSetRatings)
+  
+  invalUsers = invalUserByTS.union(invalUserByRat)
+  print "Total invalid users: ", len(invalUsers) 
+  writeList(list(invalUsers), opPrefix + '_invalid_users.txt')
+
+  filtSetRatings(uSetRatings, invalUsers, opPrefix + "_filt_set_ratings.csv")
 
 
 if __name__ == '__main__':
   main()
+
 
 
