db05cf6e3a235dc9d07612572f628df92bcccb13
Mohit Sharma <mohit.786.sharma@gmail.com>
2016-06-02 23:10:53 -0500

    added metric text based averaged

---
+++ cppsrc/avMetric.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def avgMetric(ipFNames, metric):
+  avg = 0.0
+  found = 0.0
+  for fName in ipFNames:
+    with open(fName, 'r') as f:
+      for line in f:
+        if line.startswith(metric):
+          cols = line.strip().split()
+          avg += float(cols[-1])
+          found += 1
+  print found, avg/found
+
+
+
+def main():
+  ipFNames = sys.argv[1:-1]
+  metric = sys.argv[-2]
+  avgMetric(ipFNames, metric)
+
+if __name__ == '__main__':
+  main()
+
