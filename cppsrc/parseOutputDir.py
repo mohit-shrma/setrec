1f290cb611c2d22dd7351481661c116a1d4ae5f1
mohit msi <sharm163@umn.edu>
2016-06-04 21:23:46 -0500

    fixed output dir prsing

--- cppsrc/parseOutputDir.py
+++ cppsrc/parseOutputDir.py
@@ -4,16 +4,16 @@ from os import listdir
 from os.path import isfile, join
 
 def parseFiles(dirName, prefix):
-  ipFiles = [f for f in listdir(dirName) if isfile(join(mypath, f))]
+  ipFiles = [f for f in listdir(dirName) if isfile(join(dirName, f))]
   for ipFName in ipFiles:
-    with open(ipFName, 'r') as f:
+    with open(join(dirName, ipFName), 'r') as f:
       for line in f:
         if line.startswith(prefix):
           cols = line.strip().split()
           val = float(cols[-1])
           fName = os.path.basename(ipFName)
-          params = fname.split('_')
-          print params[1], val
+          params = fName.split('_')
+          print '\t'.join(params), val
           continue
 
 def main():
@@ -22,8 +22,9 @@ def main():
   prefix = ''
   for s in sys.argv[2:]:
     prefix = prefix + s + ' '
-  
-  
+  prefix = prefix.strip()
+  #print 'prefix:', prefix 
+  parseFiles(dirName, prefix)  
 
 
 
