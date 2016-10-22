4f60f790bc3abe18b6be8bba0a20355246c21197
mohit msi <sharm163@umn.edu>
2016-10-22 11:19:25 -0500

    updated jobs gen script

--- python/grouplens/genSynJobs.py
+++ python/grouplens/genSynJobs.py
@@ -35,6 +35,34 @@ def genGridRegJobs(setrec, prefix, data, nSplits=5):
                 " > " + opDir + "/" + prefix + "_" + jobStr + ".txt"
 
 
+def genMixJobsFromParam(setrec, prefix, data, topParamFName, nSplits=5):
+  isMixRat = 1
+  with open(topParamFName, 'r') as f:
+    for line in f:
+      cols = line.strip().split()
+    for i in range(nSplits):
+      trainSet    = "split" + str(i) + "/" + "ml_set.train.syn_" + str(i) + ".lfs"
+      testSet     = "split" + str(i) + "/" + "ml_set.test.syn_" + str(i) + ".lfs"
+      valSet      = "split" + str(i) + "/" + "ml_set.val.syn_" + str(i) + ".lfs"
+      opDir = os.path.join(data, "split" + str(i), prefix)
+      if not os.path.exists(opDir):
+        os.mkdir(opDir)
+      for trainMatInd in range(5):
+        trainRatMat = "train_" + str(trainMatInd) + ".syn.csr"
+        ureg = float(cols[0])
+        ireg = float(cols[1])
+        usetbiasreg = float(cols[2])
+        gamma = float(cols[3])
+        jobStr = '_'.join(map(str, [ureg, ireg, usetbiasreg, gamma]))
+        print 'cd ' + data + ' && ', setrec, NUSERS, NITEMS, 5, 5000, SEED, \
+            ureg, ireg, usetbiasreg, 0, 0, 0, \
+            LEARNRATE, isMixRat, gamma, trainSet, testSet, valSet, RATMAT, \
+            trainRatMat, TEST_RATMAT, VAL_RATMAT, prefix, \
+            " > " + opDir + "/" + prefix + "_" + str(trainMatInd)  + \
+            "_mix_" + jobStr + ".txt"
+
+
+
 def genGridMixRegJobs(setrec, prefix, data, nSplits=5):
   isMixRat = 1
   for i in range(nSplits):
@@ -62,9 +90,11 @@ def genGridMixRegJobs(setrec, prefix, data, nSplits=5):
 def main():
   setrec = sys.argv[1]
   data   = sys.argv[2]
-  
+  topParamFName = ''
+  if len(sys.argv) > 3
+    topParamFName = sys.argv[3];
+
   prefix = os.path.basename(setrec)
-  
   
   for i in range(1,4):
     subDirName = 'buckets' + str(i)
@@ -74,7 +104,7 @@ def main():
       return
     subPref = prefix# + '_' + subDirName
     genGridRegJobs(setrec, subPref, subDir)
-    #genGridMixRegJobs(setrec, subPref + 'mix', subDir)
+    genMixJobsFromParam(setrec, subPref + 'mix', topParamFName, subDir)
 
   """
   for i in range(1,4):
@@ -85,8 +115,10 @@ def main():
       return
     subPref = prefix# + '_' + subDirName
     genGridRegJobs(setrec, subPref, subDir)
+    genMixJobsFromParam(setrec, subPref + 'mix', topParamFName, subDir)
     #genGridMixRegJobs(setrec, subPref + 'mix', subDir)
   """
+
 if __name__ == '__main__':
   main()
 
