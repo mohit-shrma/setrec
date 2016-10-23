88ad747d38fb38f64d332ee83a295b72cc566cb0
mohit msi <sharm163@umn.edu>
2016-10-23 02:24:16 -0500

    fixed jobs generation script

--- python/grouplens/genJobs.py
+++ python/grouplens/genJobs.py
@@ -39,126 +39,6 @@ VAL_RATMAT   = "val.csr"
 
 TRAIN_MIX_MATS = ['train_0.010000.csr', 'train_0.250000.csr', 'train_0.500000.csr', 'train_0.750000.csr', TRAIN_RATMAT]
 
-"""
-if not os.path.exists(OPDIR):
-  os.mkdir(OPDIR)
-
-def genRandRegJobs():
-  for dim in FACDIMS:
-    nJobs       = 0
-    while nJobs < 100:
-      ureg        = REGS[random.randint(0, len(REGS)-1)]
-      ireg        = REGS[random.randint(0, len(REGS)-1)]
-      ubiasreg    = REGS[random.randint(0, len(REGS)-1)]
-      ibiasreg    = REGS[random.randint(0, len(REGS)-1)]
-      usetbiasreg = REGS[random.randint(0, len(REGS)-1)]
-      jobStr      = '_'.join(map(str, [ureg, ireg, ubiasreg, ibiasreg,
-        usetbiasreg, dim]))
-      print SETREC, NUSERS, NITEMS, dim, 5000, SEED, \
-          ureg, ireg, usetbiasreg, ubiasreg, ibiasreg, 0, LEARNRATE, 0, 0, \
-          TRAIN_SET, TEST_SET, VAL_SET, RATMAT, TRAIN_RATMAT, TEST_RATMAT, \
-          VAL_RATMAT, PREFIX, \
-          " > " + OPDIR + "/" + PREFIX + "_" + jobStr + ".txt"
-
-      nJobs += 1
-    
-
-def genGridRegJobs():
-  for dim in FACDIMS:
-    for ureg in uREGS:
-      for ireg in iREGS:
-        for ubiasreg in ubiasREGS:
-          for ibiasreg in ibiasREGS:
-            for gbiasreg in gbiasREGS:
-              for usetbiasreg in setBiasRegs:
-              #for usetbiasreg in [0]:
-                jobStr      = '_'.join(map(str, [ureg, ireg, ubiasreg, ibiasreg, 
-                  usetbiasreg, gbiasreg, dim]))
-                print SETREC, NUSERS, NITEMS, dim, 5000, SEED, \
-                    ureg, ireg, usetbiasreg, ubiasreg, ibiasreg, gbiasreg, LEARNRATE, 0, 0, \
-                    TRAIN_SET, TEST_SET, VAL_SET, RATMAT, TRAIN_RATMAT, TEST_RATMAT, \
-                    VAL_RATMAT, PREFIX, \
-                    " > " + OPDIR + "/" + PREFIX + "_" + jobStr + ".txt"
-
-
-def genRankRegJobs():
-  for dim in FACDIMS:
-    for ureg in uREGS:
-      for ireg in iREGS:
-        for ibiasreg in ibiasREGS:
-          gamma = 0
-          #for gamma in GAMMAS:
-          #for gamma in [0]:
-          jobStr      = '_'.join(map(str, [ureg, ireg, 0, ibiasreg, 
-            0, dim]))
-          print SETREC, NUSERS, NITEMS, dim, 5000, SEED, \
-              ureg, ireg, 0, 0, ibiasreg, 0, LEARNRATE, 0, 0, \
-              TRAIN_SET, TEST_SET, VAL_SET, RATMAT, TRAIN_RATMAT, TEST_RATMAT, \
-              VAL_RATMAT, PREFIX, \
-              " > " + OPDIR + "/" + PREFIX + "_" + jobStr + ".txt"
-
-
-def genRandRankRegJobs():
-  for dim in FACDIMS:
-    nJobs       = 0
-    while nJobs < 100:
-      ureg        = REGS[random.randint(0, len(REGS)-1)]
-      ireg        = REGS[random.randint(0, len(REGS)-1)]
-      ibiasreg    = REGS[random.randint(0, len(REGS)-1)]
-      gamma       = GAMMAS[random.randint(0, len(GAMMAS)-1)]
-      jobStr      = '_'.join(map(str, [ureg, ireg, 0, ibiasreg,
-        0, gamma, dim]))
-      print SETREC, NUSERS, NITEMS, dim, 5000, SEED, \
-          ureg, ireg, 0, 0, ibiasreg, 0, LEARNRATE, 0, gamma, \
-          TRAIN_SET, TEST_SET, VAL_SET, RATMAT, TRAIN_RATMAT, TEST_RATMAT, \
-          VAL_RATMAT, PREFIX, \
-          " > " + OPDIR + "/" + PREFIX + "_" + jobStr + ".txt"
-
-      nJobs += 1
-
-
-def genJobs():
-  params = [
-      [10,0.001,0.001,0.01,0,75],
-      [0.1,0.01,0.001,10,0,1],
-      [10,0.01,0.001,0.01,0,75],
-      [10,0.001,0.001,0.01,0,100],
-      [0.1,0.01,0.001,1,0,1],
-      [10,0.01,0.01,0.01,0,75],
-      [10,0.001,0.01,0.01,0,75],
-      [1,0.01,0.001,0.01,0,15],
-      [0.1,0.01,0.01,10,0,1],
-      [1,0.01,0.001,0.01,0,75],
-      [10,0.001,0.01,0.01,0,100],
-      [1,0.01,0.001,0.01,0,100],
-      [10,0.01,0.001,0.01,0,100],
-      [1,0.001,0.001,0.01,0,15],
-      [1,0.001,0.001,0.01,0,100],
-      [10,0.001,0.001,0.01,0,15],
-      [1,0.001,0.001,0.01,0,75],
-      [1,0.001,0.001,0.01,0,50],
-      [1,0.01,0.001,0.01,0,50],
-      [0.1,0.001,0.001,1,0,1]
-      ]
-  for param in params[:5]:
-    ureg  = param[0]
-    ireg  = param[1]
-    ubiasreg = param[2]
-    ibiasreg = param[3]
-    usetbiasreg = param[4]
-    dim   = param[5]
-    for trainMat in ['train_0.1.csr', 'train_0.25.csr', 'train_0.5.csr',
-        'train_0.75.csr', 'train.csr']:
-      jobStr      = '_'.join(map(str, [ureg, ireg, ubiasreg, ibiasreg, 
-        0, dim, trainMat]))
-      trainMat = DATA + trainMat
-      print SETREC, NUSERS, NITEMS, dim, 5000, SEED, \
-          ureg, ireg, usetbiasreg, ubiasreg, ibiasreg, 0, LEARNRATE, 0, 0, \
-          TRAIN_SET, TEST_SET, VAL_SET, RATMAT, trainMat, TEST_RATMAT, \
-          VAL_RATMAT, PREFIX, \
-          " > " + OPDIR + "/" + PREFIX + "_" + jobStr + ".txt"
-"""
-
 def genGridRegJobs(setrec, prefix, data):
   opDir = os.path.join(data, prefix)
   if not os.path.exists(opDir):
@@ -166,9 +46,9 @@ def genGridRegJobs(setrec, prefix, data):
   for dim in FACDIMS:
     for ureg in REGS:
       for ireg in REGS:
-        for usetbiasreg in REGS:#setBiasRegs:
-          for gamma in [0.1, 0.25, 0.5]:
-            jobStr = '_'.join(map(str, [ureg, ireg, usetbiasreg, gamma]))
+        for usetbiasreg in [0]:#REGS:#setBiasRegs:
+          for gamma in [0]:#[0.1, 0.25, 0.5]:
+            jobStr = '_'.join(map(str, [dim, ureg, ireg, usetbiasreg, gamma]))
             print 'cd ' + data + ' && ', setrec, NUSERS, NITEMS, dim, 5000, SEED, \
                 ureg, ireg, usetbiasreg, 0, 0, 0, \
                 LEARNRATE, 0, gamma, TRAIN_SET, TEST_SET, VAL_SET, RATMAT, \
@@ -205,7 +85,7 @@ def main():
   data = sys.argv[2]
   prefix = os.path.basename(setrec)
 
-  for i in range(1,6):
+  for i in range(1,2):
     subDirName = 'split' + str(i)
     subDir = os.path.join(data, subDirName)
     if not os.path.exists(subDir):
